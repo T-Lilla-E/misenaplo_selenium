cab1061de24951cc42fd0b068b2a602414b4fe8e
Lilla Tóth <toth.lilla.e@gmail.com>
2025-05-10 12:11:08 +0200

    Revert "configuration for utf-8"
    This reverts commit 8680df712e513661193878870d1575b3f1f33036.

--- src/test/java/PageBase.java
+++ src/test/java/PageBase.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PageBase {
     
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-
-    protected By bodyLocator = By.tagName("body");
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     private String[] urls;
 
@@ -18,19 +16,14 @@ public class PageBase {
         this.wait = new WebDriverWait(driver, 10);
     }
 
-    // public PageBase(WebDriver driver, String[] urls){
-    //     this.driver = driver;
-    //     this.wait = new WebDriverWait(driver, 10);
-    //     this.urls = urls;
-    // }
+    public PageBase(WebDriver driver, String[] urls){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+        this.urls = urls;
+    }
 
     protected WebElement waitAndReturnElement(By locator){
         this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         return this.driver.findElement(locator);
-    }
-
-    public void pressButton(By locator){
-        WebElement buttonElement = waitAndReturnElement(locator);
-        buttonElement.click();
     }
 }
--- src/test/java/ProfilePage.java
+++ src/test/java/ProfilePage.java
@@ -1,20 +1,9 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class ProfilePage extends LoggedInPageBase {
-
-    private By titleLocator = By.xpath("//div[span[contains(., \"Felhasználói profil\")]]");
-
     
     public ProfilePage(WebDriver driver){
         super(driver);
-        this.driver.get("https://misenaplo.hu/profile");
-        waitAndReturnElement(bodyLocator);
-    }
-
-    public String getTitle() {
-        WebElement titleElement = waitAndReturnElement(titleLocator);
-        return titleElement.getText();
+        //navigation
     }
 }
