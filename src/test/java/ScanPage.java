3c1a9f5c954d22d8d2923aa48080454317360abb
Lilla Tóth <toth.lilla.e@gmail.com>
2025-05-12 21:54:25 +0200

    scanPage static testing started

--- src/test/java/ScanPage.java
+++ src/test/java/ScanPage.java
@@ -1,12 +1,39 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ScanPage extends LoggedInPageBase {
+
+    private By headerLocator = By.tagName("header");
+    private By fieldsetLocator = By.tagName("fieldset");
+    private By imageLocator = By.xpath("//div[contains(@class, \"image--cover\")]");
+    private By footerLocator = By.tagName("footer");
+
     
     public ScanPage(WebDriver driver){
         super(driver);
         this.titleLocator = By.xpath("//div[span[contains(., \"Csoportba rögzítés\")]]");
         this.driver.get(config.getBaseUrl() + "/scanTask");
         waitAndReturnElement(bodyLocator);
+    }
+
+    public boolean hasHeader(){
+        WebElement header = waitAndReturnElement(headerLocator);
+        return header.isDisplayed();
+    }
+
+    public boolean hasDropdown(){
+        WebElement dropdown = waitAndReturnElement(fieldsetLocator);
+        return dropdown.isDisplayed();
+    }
+
+    public boolean hasImage(){
+        WebElement image = waitAndReturnElement(imageLocator);
+        return image.isDisplayed();
+    }
+
+    public boolean hasFooter(){
+        WebElement footer = waitAndReturnElement(footerLocator);
+        return footer.isDisplayed();
     }
 }
