064a40245de71177c28cab454c51341c973136d8
Lilla Tóth <toth.lilla.e@gmail.com>
2025-05-19 22:11:12 +0200

    navigation methods in LoggedInPageBase

--- src/test/java/LoggedInPageBase.java
+++ src/test/java/LoggedInPageBase.java
@@ -6,7 +6,11 @@ public abstract class LoggedInPageBase extends PageBase {
 
     protected By logoutLocator = By.xpath("//button[span[contains(., \"Kijelentkezés\")]]");
 
+    protected By profileLocator = By.xpath("//a[@href=\"/profile\"]");
     protected By groupsLocator = By.xpath("//a[@href=\"/groups\"]");
+    protected By usersLocator = By.xpath("//a[@href=\"/users\"]");
+    protected By parishesLocator = By.xpath("//a[@href=\"/parishes\"]");
+    protected By scanTaskLocator = By.xpath("//a[@href=\"/scanTask\"]");
     
     private By locatorOfMirjam = By.xpath("//h1[contains(., \"Betlehemi Szent Mirjam\")]");
     
@@ -21,9 +25,33 @@ public abstract class LoggedInPageBase extends PageBase {
         return new LandingPage(this.driver);
     }
 
+    public ProfilePage navigateToProfilePage(){
+        pressButtonFromSideMenu(profileLocator);
+        this.wait.until(ExpectedConditions.urlContains("/profile"));
+        return new ProfilePage(this.driver);
+    }
+
     public GroupsPage navigateToGroupsPage(){
         pressButtonFromSideMenu(groupsLocator);
         this.wait.until(ExpectedConditions.urlContains("/groups"));
         return new GroupsPage(this.driver);
+    }
+
+    public UsersPage navigateToUsersPage(){
+        pressButtonFromSideMenu(usersLocator);
+        this.wait.until(ExpectedConditions.urlContains("/users"));
+        return new UsersPage(this.driver);
+    }
+
+    public ParishesPage navigateToParishesPage(){
+        pressButtonFromSideMenu(parishesLocator);
+        this.wait.until(ExpectedConditions.urlContains("/parishes"));
+        return new ParishesPage(this.driver);
+    }
+
+    public ScanPage navigateToScanPage(){
+        pressButtonFromSideMenu(scanTaskLocator);
+        this.wait.until(ExpectedConditions.urlContains("/scanTask"));
+        return new ScanPage(this.driver);
     }
 }
